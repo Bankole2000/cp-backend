a0231d2f4a7c7ec8f47580d774f22cc1679401d4
Bankole Esan <techybanky@gmail.com>
2023-04-17 05:28:20 +0100

    fix(staging): setting up health check

--- packages/auth-service/src/controllers/test.controllers.ts
+++ packages/auth-service/src/controllers/test.controllers.ts
@@ -5,14 +5,30 @@ import { getServiceQueues } from '../services/events.service';
 import { config } from '../utils/config';
 
 export const testEndpointHandler = async (req: Request, res: Response) => {
+  console.log('Connecting to redis');
   await req.redis.client.connect();
+  console.log('Redis connected');
+  console.log('Fetching services from redis');
   const services = await req.redis.client.hGetAll(`${config.redisConfig.scope}-services`);
+  console.log('Services fetched');
+  console.log('Fetching comms service from redis');
   const commsService = await req.redis.client.hGet(`${config.redisConfig.scope}-services`, 'comms-service');
+  console.log('Comms service fetched');
+  console.log('Fetching auth service from redis');
   const authService = await req.redis.client.hGet(`${config.redisConfig.scope}-services`, config.self.serviceName || '');
+  console.log('Auth service fetched');
+  console.log('Fetching queues from redis');
   const queues = await req.redis.client.sMembers(`${config.redisConfig.scope}-queues`);
+  console.log('Queues fetched');
+  console.log('Fetching with no key from redis');
   const missingKey = await req.redis.client.hGetAll(`${config.redisConfig.scope}`);
+  console.log('No key fetched');
+  console.log('disconnecting redis');
   await req.redis.client.disconnect();
+  console.log('redis disconnected');
+  console.log('Testing getting all queues');
   const allQueues = await getServiceQueues(req.redis, config.redisConfig.scope, []);
+  console.log('all queues fetched');
   const someQueues = await getServiceQueues(req.redis, config.redisConfig.scope, ['comms', 'event', 'transaction']);
   if (req.query.query === 'error') {
     const sr = new ServiceResponse('Error', null, false, 500, 'Error', null, null);
